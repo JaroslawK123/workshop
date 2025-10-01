3a19fb1d62a39b36da1ae2915a4a28641b84aa80
JaroslawK123 <jaroslawk@outlook.com>
2025-10-01 10:23:51 +0000

    aaa

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_service_plan" "example" {
 resource "azurerm_linux_web_app" "example" {
   name                = "namejaroslawk643678954" 
   location            = "westeurope" 
-  resource_group_name = "uers01"
+  resource_group_name = "user01"
   service_plan_id     = azurerm_service_plan.example.id
   site_config {}
 }
